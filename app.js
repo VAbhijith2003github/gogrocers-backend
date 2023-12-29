386eca15a2b7789c3dd57b21c141360f060f941a
V Abhijith <119759593+VAbhijith2003github@users.noreply.github.com>
2023-12-29 12:39:08 +0530

    add app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const port = 3001;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
